ce58931921d04f99b074e1eeab224fb8f0331a7e
unknown <SENA@PITAPRTNNFSD276.FormacionSena.RED>
2022-03-02 17:33:08 -0500

    avance en la logica

--- js/carrito-compras.js
+++ js/carrito-compras.js
@@ -3,29 +3,27 @@
 let contador = 1;
 let numUnidades = "";
 let numProductos
-let precio = 480000;
+let precio = 480000/* document.getElementById('precioEstable').innerHTML */;
 let costoTotal
 
 function aumentar(){
+
     if(contador < 10) {
         numUnidades = document.getElementById('numUnidades').value = ++contador;
 
-        numProductos = document.getElementById('numProductos');
-        numProductos.innerHTML = contador;
+        numProductos = document.getElementById('numProductos').innerHTML = contador;
 
-        costoTotal = document.getElementById('precio');
-        costoTotal.innerHTML = precio * contador
+        costoTotal = document.getElementById('subtotal').innerHTML = precio * contador;
     }
 }
 
 function disminuir(){
+
     if(contador > 1) {
-        numUnidades = document.getElementById('numUnidades').value = --contador;
+        numUnidades = document.getElementById('numUnidades').value = contador--;
 
-        numProductos = document.getElementById('numProductos');
-        numProductos.innerHTML = contador;
+        numProductos = document.getElementById('numProductos').innerHTML = contador;
 
-        costoTotal = document.getElementById('precio');
-        costoTotal.innerHTML = precio * contador - contador
+        costoTotal = document.getElementById('subtotal').innerHTML = precio * contador;
     }
 }
