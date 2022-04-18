dc46a4d449b2a7c4293adba34c8b3664e9d0005c
B0ssMan69 <jafajardo8845@misena.edu.co>
2022-04-18 17:14:17 -0500

    Terminando homecenter

--- js/homecenter.js
+++ js/homecenter.js
@@ -1,18 +1,47 @@
 "use strict"
 
+let botonOpen = document.getElementById("chat-box-disable");
+let botonCerrar = document.getElementById("botonCerrar");
+let botonOpenHeader = document.getElementById("header-movil-open");
+let botonCerrarHeader = document.getElementById("boton-cerrar-header");
+let Header = document.getElementById("header");
+
+botonOpen.addEventListener("click", dezplegarCaja);
+botonCerrar.addEventListener("click", cerrarCaja);
+
 function dezplegarCaja(){
 
-    let chatBoxIn = document.getElementById("chat-box");
-        chatBoxIn.classList.add("chat-box-in");
-    let chatBoxOut = document.getElementById("chat-box");
-        chatBoxOut.classList.remove("chat-box-out");
+    botonOpen.classList.add("chat-box-disable-in");
+    botonOpen.classList.remove("chat-box-disable-out");
+    document.getElementById("chat-box").classList.add("chat-box-in");
+    document.getElementById("chat-box").classList.remove("chat-box-out");
 
-    let headerChatBox = document.getElementById("all-content");
-        headerChatBox.classList.add("all-content-in");
-};
+}
 
 function cerrarCaja(){
     
+    botonOpen.classList.remove("chat-box-disable-in");
+    botonOpen.classList.add("chat-box-disable-out");
     document.getElementById("chat-box").classList.remove("chat-box-in");
     document.getElementById("chat-box").classList.add("chat-box-out");
 };
+
+botonOpenHeader.addEventListener("click", dezplegarCajaHeader);
+botonCerrarHeader.addEventListener("click", cerrarCajaHeader);
+
+function dezplegarCajaHeader(){
+
+    document.getElementById("header-movil").classList.add("contenedor-barra-busqueda-movil");
+
+    document.getElementById("header-movil").classList.remove("contenedor-barra-busqueda-movil-out");
+
+    document.getElementById("header").style.display="none";
+}
+
+function cerrarCajaHeader(){
+    document.getElementById("header-movil").classList.remove("contenedor-barra-busqueda-movil");
+
+    document.getElementById("header-movil").classList.add("contenedor-barra-busqueda-movil-out");
+
+    document.getElementById("header").style.display="flex";
+}
